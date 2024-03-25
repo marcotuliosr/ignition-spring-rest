1d80de5c79aad82f5f7d37a4aedf4135a42068ee
marcotuliosr <50932692+marcotuliosr@users.noreply.github.com>
2024-03-25 16:00:58 -0300

    initial commit

---
+++ src/test/java/api/rest/transito/transitoapi/TransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.rest.transito.transitoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

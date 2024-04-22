dd94e1501f23b7fa4b061debc9095b3d5012a533
marcotuliosr <50932692+marcotuliosr@users.noreply.github.com>
2024-04-22 10:37:23 -0300

    feat/injection and use SDJ

--- src/main/java/api/rest/transito/transitoapi/api/controller/ProprietarioController.java
+++ src/main/java/api/rest/transito/transitoapi/api/controller/ProprietarioController.java
@@ -1,26 +1,20 @@
 package api.rest.transito.transitoapi.api.controller;
 
 import api.rest.transito.transitoapi.domain.model.Proprietario;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import api.rest.transito.transitoapi.domain.repository.ProprietarioRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private ProprietarioRepository proprietarioRepository;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        return manager.createQuery("from Proprietario", Proprietario.class).getResultList();
-
-//        TypedQuery<Proprietario> query = manager
-//                .createQuery("from Proprietario", Proprietario.class);
-//        return query.getResultList();
-
+        return proprietarioRepository.findAll();
     }
 }

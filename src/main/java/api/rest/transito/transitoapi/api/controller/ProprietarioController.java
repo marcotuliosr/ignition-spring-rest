eef9d6d90b5248b6c3d0ff421ff2ab40c006eaa4
marcotuliosr <50932692+marcotuliosr@users.noreply.github.com>
2024-04-22 06:58:39 -0300

    feat/creating the first JPQL query

--- src/main/java/api/rest/transito/transitoapi/api/controller/ProprietarioController.java
+++ src/main/java/api/rest/transito/transitoapi/api/controller/ProprietarioController.java
@@ -1,29 +1,26 @@
 package api.rest.transito.transitoapi.api.controller;
 
 import api.rest.transito.transitoapi.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProprietarioController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("Marco Túlio Da Silva Rocha");
-        proprietario1.setEmail("marcotuliopessoal@gmail.com");
-        proprietario1.setTelefone("(31) 99999-9999");
+        return manager.createQuery("from Proprietario", Proprietario.class).getResultList();
 
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Maria Clara Da Silva Rocha");
-        proprietario2.setEmail("mariaclarasilva@gmail.com");
-        proprietario2.setTelefone("(31) 99999-88888");
+//        TypedQuery<Proprietario> query = manager
+//                .createQuery("from Proprietario", Proprietario.class);
+//        return query.getResultList();
 
-        return Arrays.asList(proprietario1, proprietario2);
     }
 }

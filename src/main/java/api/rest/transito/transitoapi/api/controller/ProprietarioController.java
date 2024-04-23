1083e08ee6b4241d02cb143200a6940481d35c01
marcotuliosr <50932692+marcotuliosr@users.noreply.github.com>
2024-04-23 16:39:01 -0300

    feat/created post method

--- src/main/java/api/rest/transito/transitoapi/api/controller/ProprietarioController.java
+++ src/main/java/api/rest/transito/transitoapi/api/controller/ProprietarioController.java
@@ -3,11 +3,9 @@ package api.rest.transito.transitoapi.api.controller;
 import api.rest.transito.transitoapi.domain.model.Proprietario;
 import api.rest.transito.transitoapi.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,5 +29,11 @@ public class ProprietarioController {
                 .orElse(ResponseEntity.notFound().build());
 
 
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adcionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 }
